b33921d23b471cdb37157b8f8edcb0c9653f4c24
James Nguyen <james.n@ischool.berkeley.edu>
2018-06-28 09:14:38 -0700

    Add project files.

---
+++ profile_manager/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\janguy\source\repos\profile_manager\profile_manager\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ profile_manager/profile_manager/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for profile_manager.
+"""
